8d30fdff82f25dd178e468499d3eaefc1d6fae4b
ᴜɴᴋɴᴡᴏɴ <u@gogs.io>
2021-06-08 02:22:12 +0800

    session: support custom read and write logic for session ID (#7)
    Co-authored-by: E99p1ant <i@github.red>

--- session_test.go
+++ session_test.go
@@ -31,5 +31,57 @@ func TestSessioner(t *testing.T) {
 	f.ServeHTTP(resp, req)
 
 	want := fmt.Sprintf("flamego_session=%s; Path=/; HttpOnly; SameSite=Lax", resp.Body.String())
-	assert.Equal(t, want, resp.Header().Get("Set-Cookie"))
+	cookie := resp.Header().Get("Set-Cookie")
+	assert.Equal(t, want, cookie)
+
+	// Make a request again using the same session ID
+	resp = httptest.NewRecorder()
+	req, err = http.NewRequest(http.MethodGet, "/", nil)
+	assert.Nil(t, err)
+
+	req.Header.Set("Cookie", cookie)
+	f.ServeHTTP(resp, req)
+
+	got := fmt.Sprintf("flamego_session=%s; Path=/; HttpOnly; SameSite=Lax", resp.Body.String())
+	assert.Equal(t, cookie, got)
+}
+
+func TestSessioner_Header(t *testing.T) {
+	f := flamego.NewWithLogger(&bytes.Buffer{})
+	f.Use(Sessioner(
+		Options{
+			ReadIDFunc: func(r *http.Request) string {
+				return r.Header.Get("Session-Id")
+			},
+			WriteIDFunc: func(w http.ResponseWriter, r *http.Request, sid string, created bool) {
+				if created {
+					r.Header.Set("Session-Id", sid)
+				}
+				w.Header().Set("Session-Id", sid)
+			},
+		},
+	))
+	f.Get("/", func(c flamego.Context, session Session, store Store) string {
+		_ = store.GC(c.Request().Context())
+		return session.ID()
+	})
+
+	resp := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodGet, "/", nil)
+	assert.Nil(t, err)
+
+	f.ServeHTTP(resp, req)
+
+	sid := resp.Header().Get("Session-Id")
+	assert.Equal(t, resp.Body.String(), sid)
+
+	// Make a request again using the same session ID
+	resp = httptest.NewRecorder()
+	req, err = http.NewRequest(http.MethodGet, "/", nil)
+	assert.Nil(t, err)
+
+	req.Header.Set("Session-Id", sid)
+	f.ServeHTTP(resp, req)
+
+	assert.Equal(t, sid, resp.Body.String())
 }
