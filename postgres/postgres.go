95acb2fc6a48847a5a36ca277a847cf78d0ff1ff
Joe Chen <jc@unknwon.io>
2023-03-10 10:53:28 +0800

    mod: bump to github.com/jackc/pgx/v5 (#73)

--- postgres/postgres.go
+++ postgres/postgres.go
@@ -10,8 +10,8 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/jackc/pgx/v4"
-	"github.com/jackc/pgx/v4/stdlib"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/stdlib"
 	"github.com/pkg/errors"
 
 	"github.com/flamego/session"
