201e953d7fa3fb09e197c2962e7d186b6238c659
Joe Chen <jc@unknwon.io>
2023-07-12 22:20:13 -0400

    sqlite: initial implementation

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.8.4
 	go.mongodb.org/mongo-driver v1.11.7
+	modernc.org/sqlite v1.24.0
 )
 
 require (
@@ -20,11 +21,13 @@ require (
 	github.com/charmbracelet/log v0.2.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
-	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
@@ -34,6 +37,7 @@ require (
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.6.1 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -41,9 +45,19 @@ require (
 	github.com/xdg-go/stringprep v1.0.3 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/mod v0.8.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	golang.org/x/tools v0.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	lukechampine.com/uint128 v1.2.0 // indirect
+	modernc.org/cc/v3 v3.40.0 // indirect
+	modernc.org/ccgo/v3 v3.16.13 // indirect
+	modernc.org/libc v1.22.5 // indirect
+	modernc.org/mathutil v1.5.0 // indirect
+	modernc.org/memory v1.5.0 // indirect
+	modernc.org/opt v0.1.3 // indirect
+	modernc.org/strutil v1.1.3 // indirect
+	modernc.org/token v1.0.1 // indirect
 )
---
+++ sqlite/sqlite_test.go
@@ -0,0 +1,184 @@
+// Copyright 2023 Flamego. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package sqlite
+
+import (
+	"bytes"
+	"context"
+	"database/sql"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/flamego/flamego"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/flamego/session"
+)
+
+func newTestDB(t *testing.T, ctx context.Context) (testDB *sql.DB, cleanup func() error) {
+	dbname := filepath.Join(os.TempDir(), fmt.Sprintf("flamego-test-sessions-%d.db", time.Now().Unix()))
+	testDB, err := sql.Open("sqlite", dbname)
+	if err != nil {
+		t.Fatalf("Failed to open test database: %v", err)
+	}
+
+	t.Cleanup(func() {
+		defer func() { _ = testDB.Close() }()
+
+		if t.Failed() {
+			t.Logf("DATABASE %s left intact for inspection", dbname)
+			return
+		}
+
+		err := testDB.Close()
+		if err != nil {
+			t.Fatalf("Failed to close test connection: %v", err)
+		}
+		err = os.Remove(dbname)
+		if err != nil {
+			t.Fatalf("Failed to delete test database: %v", err)
+		}
+	})
+	return testDB, func() error {
+		if t.Failed() {
+			return nil
+		}
+
+		_, err = testDB.ExecContext(ctx, `DELETE FROM sessions`)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func TestSQLiteStore(t *testing.T) {
+	ctx := context.Background()
+	db, cleanup := newTestDB(t, ctx)
+	t.Cleanup(func() {
+		assert.Nil(t, cleanup())
+	})
+
+	f := flamego.NewWithLogger(&bytes.Buffer{})
+	f.Use(session.Sessioner(
+		session.Options{
+			Initer: Initer(),
+			Config: Config{
+				nowFunc:   time.Now,
+				db:        db,
+				InitTable: true,
+			},
+		},
+	))
+
+	f.Get("/set", func(s session.Session) {
+		s.Set("username", "flamego")
+	})
+	f.Get("/get", func(s session.Session) {
+		sid := s.ID()
+		assert.Len(t, sid, 16)
+
+		username, ok := s.Get("username").(string)
+		assert.True(t, ok)
+		assert.Equal(t, "flamego", username)
+
+		s.Delete("username")
+		_, ok = s.Get("username").(string)
+		assert.False(t, ok)
+
+		s.Set("random", "value")
+		s.Flush()
+		_, ok = s.Get("random").(string)
+		assert.False(t, ok)
+	})
+	f.Get("/destroy", func(c flamego.Context, session session.Session, store session.Store) error {
+		return store.Destroy(c.Request().Context(), session.ID())
+	})
+
+	resp := httptest.NewRecorder()
+	req, err := http.NewRequest("GET", "/set", nil)
+	assert.Nil(t, err)
+
+	f.ServeHTTP(resp, req)
+	assert.Equal(t, http.StatusOK, resp.Code)
+
+	cookie := resp.Header().Get("Set-Cookie")
+
+	resp = httptest.NewRecorder()
+	req, err = http.NewRequest("GET", "/get", nil)
+	assert.Nil(t, err)
+
+	req.Header.Set("Cookie", cookie)
+	f.ServeHTTP(resp, req)
+	assert.Equal(t, http.StatusOK, resp.Code)
+
+	resp = httptest.NewRecorder()
+	req, err = http.NewRequest("GET", "/destroy", nil)
+	assert.Nil(t, err)
+
+	req.Header.Set("Cookie", cookie)
+	f.ServeHTTP(resp, req)
+	assert.Equal(t, http.StatusOK, resp.Code)
+}
+
+func TestSQLiteStore_GC(t *testing.T) {
+	return
+	ctx := context.Background()
+	db, cleanup := newTestDB(t, ctx)
+	t.Cleanup(func() {
+		assert.Nil(t, cleanup())
+	})
+
+	now := time.Now()
+	store, err := Initer()(ctx,
+		Config{
+			nowFunc:   func() time.Time { return now },
+			db:        db,
+			Lifetime:  time.Second,
+			InitTable: true,
+		},
+	)
+	assert.Nil(t, err)
+
+	now = now.Add(3 * time.Second)
+	sess1, err := store.Read(ctx, "1")
+	assert.Nil(t, err)
+	err = store.Save(ctx, sess1)
+	assert.Nil(t, err)
+	now = now.Add(-3 * time.Second)
+
+	now = now.Add(-2 * time.Second)
+	sess2, err := store.Read(ctx, "2")
+	assert.Nil(t, err)
+
+	sess2.Set("name", "flamego")
+	err = store.Save(ctx, sess2)
+	assert.Nil(t, err)
+
+	// Read on an expired session should wipe data but preserve the record.
+	now = now.Add(2 * time.Second)
+	tmp, err := store.Read(ctx, "2")
+	assert.Nil(t, err)
+	assert.Nil(t, tmp.Get("name"))
+
+	now = now.Add(-2 * time.Second)
+	sess3, err := store.Read(ctx, "3")
+	assert.Nil(t, err)
+	err = store.Save(ctx, sess3)
+	assert.Nil(t, err)
+
+	now = now.Add(3 * time.Second)
+	err = store.GC(ctx) // sess3 should be recycled
+	assert.Nil(t, err)
+
+	assert.True(t, store.Exist(ctx, "1"))
+	assert.False(t, store.Exist(ctx, "2"))
+	assert.False(t, store.Exist(ctx, "3"))
+}
